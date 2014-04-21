3c1baf31f8be036845451d5f942b407d6c222ed0
vagrant <vagrant@kellogg-dev-box>
2014-04-21 13:42:02 +0000

    dice roll code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+@images  = ["http://kiei925.com/images/dice/1.png/", "http://kiei925.com/images/dice/2.png/", "http://kiei925.com/images/dice/3.png/"]
+            @random_no = rand(3)
+            @random_image = @images[@random_no]
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
